e85fd331bd4a363685efae631eb52e03af72ae65
Joy_0130 <gm10290014@gmail.com>
2025-07-26 15:00:10 +0800

    update news.py

---
+++ news.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+import json
+url = "https://api.cnyes.com/media/api/v1/newslist/category/headline" #新聞網址
+payload = {
+    "page":2,
+    "itCategoryHeadline":1,
+    "limit":30,
+    "startAt":1752645067,
+    "endAt":1753509067
+}
+res = requests.get(url,params = payload)
+jd = json.loads(res.text)
+df = pd.DataFrame(jd['items']['data'])
+df = df[['newsId','title','summary']]
+df['link'] = df['newsId'].apply(lambda x: 'https://m.cnyes.com/news/id/' + str(x))
+df.to_csv('news.csv', encoding = 'utf-8-sig')
+print(df)
