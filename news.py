01661c1dd15a1a5e7bc478c69454f21115f841d2
Joy_0130 <gm10290014@gmail.com>
2025-08-02 10:25:38 +0800

    Updade news.py_V3

--- news.py
+++ news.py
@@ -2,9 +2,10 @@ import requests
 import pandas as pd
 import datetime as dt
 import json
+data = []
 url = "https://api.cnyes.com/media/api/v1/newslist/category/headline"
 payload = {
-    "page":2,
+    "page":1,
     "itCategoryHeadline":1,
     "limit":30,
     "startAt":int((dt.datetime.today()- dt.timedelta(days=11)).timestamp()),
@@ -12,6 +13,15 @@ payload = {
 }
 res = requests.get(url,params = payload)
 jd = json.loads(res.text)
+data.append(pd.DataFrame(jd['items']['data']))
+
+for i in range(2,jd['items']['last_page'] +1):
+    print("i = ", i)
+    payload['page'] = i
+    res = requests.get(url,params = payload)
+    jd = json.loads(res.text)
+    data.append(pd.DataFrame(jd['items']['data']))
+
 df = pd.DataFrame(jd['items']['data'])
 df = df[['newsId','title','summary']]
 df['link'] = df['newsId'].apply(lambda x: 'https://m.cnyes.com/news/id/' + str(x))
